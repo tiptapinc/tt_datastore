e89c74790684e587c6f2e2b7a395a26a3a55592f
puerile22 <puerile22@gmail.com>
2022-05-12 10:25:12 -0700

    More update

--- tt_datastore/datastore.py
+++ tt_datastore/datastore.py
@@ -53,14 +53,14 @@ class Datastore(object):
     def read(self, key, **kwargs):
         try:
             result = self.collection.get(key, **kwargs)
-            return result.content_as
+            return result.value
         except couchbase.exceptions.DocumentNotFoundException:
             return
 
     def read_with_cas(self, key, **kwargs):
         try:
             result = self.collection.get(key, **kwargs)
-            return result.content_as, result.cas
+            return result.value, result.cas
         except couchbase.exceptions.DocumentNotFoundException:
             return None, None
 
@@ -68,7 +68,7 @@ class Datastore(object):
         result = self.collection.get_and_lock(
             key, datetime.timedelta(seconds=ttl), **kwargs
         )
-        return result.content_as, result.cas
+        return result.value, result.cas
 
     def unlock(self, key, cas):
         try:
